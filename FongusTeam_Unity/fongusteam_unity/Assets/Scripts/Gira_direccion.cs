b700a9bbe962235138571bb1c9e4d541aeabcf4d
DarknusWolf <girelagarcia.javier29@gmail.com>
2015-11-24 20:49:47 +0100

    Muerte Pinchos y otras cosas

---
+++ FongusTeam_Unity/fongusteam_unity/Assets/Scripts/Gira_direccion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gira_direccion : MonoBehaviour {
+    public Rigidbody2D rg;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D objeto){
+        if (objeto.transform.tag == "Esq_izq"){
+            //rg.transform.rotation = new Vector3 (rg.transform.rotation.x, rg.transform.rotation.y, rg.transform.rotation.z + 90);
+        }
+
+        if (objeto.transform.tag == "Esq_der"){
+
+        }
+    }
+}
