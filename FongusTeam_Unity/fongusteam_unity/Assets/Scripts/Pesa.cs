eb1d2949677b19ccb65d98db2c57e2680c5bc8cf
DarknusWolf <girelagarcia.javier29@gmail.com>
2015-11-24 20:52:30 +0100

    muerte

---
+++ FongusTeam_Unity/fongusteam_unity/Assets/Scripts/Pesa.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pesa : MonoBehaviour {
+    Rigidbody2D rg;
+	public string nombre = "Player";
+
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody2D>();
+        rg.gravityScale = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D objeto){
+		string etiqueta = objeto.transform.tag;
+		Debug.Log (etiqueta);
+		if (etiqueta == nombre) {
+			rg.gravityScale = 1;
+		}
+	}
+}
